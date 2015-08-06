12d5c39457e700676d9e8c53445d1ed5025ad7fd
stevieing <si5@sanger.ac.uk>
2015-08-06 11:16:55 +0100

    Modified migrations to fix bug with references.

--- db/migrate/20150727073844_remove_labware_reference_from_coordinates.rb
+++ db/migrate/20150727073844_remove_labware_reference_from_coordinates.rb
@@ -1,5 +1,5 @@
 class RemoveLabwareReferenceFromCoordinates < ActiveRecord::Migration
   def change
-    remove_reference :coordinates, :labwares, index: true, foreign_key: true
+    remove_reference :coordinates, :labware, index: true, foreign_key: true
   end
 end
