781aecf9b36ab2053973df584f2dbc18ee9f1087
stevieing <si5@sanger.ac.uk>
2015-08-18 07:56:00 +0100

    modified migrations.

--- db/migrate/20150410104157_create_labwares.rb
+++ db/migrate/20150410104157_create_labwares.rb
@@ -5,7 +5,6 @@ class CreateLabwares < ActiveRecord::Migration
       t.datetime :deleted_at
       t.integer :histories_count, default: 0
       t.references :location, index: true, foreign_key: true
-      #t.references :previous_location, index: true, foreign_key: true
       t.timestamps null: false
     end
   end
--- db/migrate/20150727101943_remove_previous_location_reference_from_labwares.rb
+++
@@ -1,5 +0,0 @@
-class RemovePreviousLocationReferenceFromLabwares < ActiveRecord::Migration
-  def change
-    #remove_reference :labwares, :previous_location, index: true, foreign_key: true
-  end
-end
