411daf8dc2734c366c698211285111f883ec5ec2
stevieing <si5@sanger.ac.uk>
2016-04-19 09:33:34 +0100

    Added hack to ensure deployment does not fails on cgap rake task.

--- config/environments/cgap.rb
+++ config/environments/cgap.rb
@@ -39,6 +39,4 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.autoload_paths += %W(#{config.root}/lib/cgap #{config.root}/lib/cgap/migrations)
-
 end
