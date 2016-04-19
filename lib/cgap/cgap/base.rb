4316762b3814143e6c9e4667ed351f6305a1e6cc
stevieing <si5@sanger.ac.uk>
2016-04-19 08:22:45 +0100

    Commented out establish connection in Cgap::Base as it is causing an error.

--- lib/cgap/cgap/base.rb
+++ lib/cgap/cgap/base.rb
@@ -1,6 +1,9 @@
 class Cgap::Base < ActiveRecord::Base
   self.abstract_class = true
 
-  establish_connection  Rails.configuration.database_configuration["cgap"]
+  # TODO: The following statement causes errors when the rake task is run and when the app is
+  # deployed.
+  # establish_connection  Rails.configuration.database_configuration["cgap"]
+  # establish_connection  :cgap
 
 end
