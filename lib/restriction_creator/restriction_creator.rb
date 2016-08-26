adb3ee9d8a00684b850e169be42903e6b3ccc3fe
Yana Proskurina <yana.proskurina@sanger.ac.uk>
2016-08-26 08:59:17 +0100

    Added rake task to create restrictions, added tests, deleted label_printing

--- lib/restriction_creator/restriction_creator.rb
+++ lib/restriction_creator/restriction_creator.rb
@@ -16,12 +16,21 @@ private
     restrictions.each do |k, restriction|
       create_restriction(restriction)
     end
-    binding.pry
   end
 
   def create_restriction(restriction)
-    Restriction.create(restriction.except("location_types_restrictions").merge("location_type": LocationType.find_by_name(restriction["location_type"])))
+    type  = restriction["type"] || "Restriction"
+    location_type = LocationType.find_or_create_by(name: restriction["location_type"])
+    location_types_restrictions = restriction.delete("location_types_restrictions")
+    new_restriction = type.constantize.create(restriction.merge("location_type": location_type))
+    create_location_types_restrictions(new_restriction, location_types_restrictions) if location_types_restrictions.present?
   end
 
-  
+  def create_location_types_restrictions(restriction, location_types_restrictions)
+    location_types_restrictions.each do |location_type_name|
+      location_type = LocationType.find_or_create_by(name: location_type_name)
+      LocationTypesRestriction.create("location_type_id" => location_type.id, "restriction_id" => restriction.id)
+    end
+  end
+
 end
