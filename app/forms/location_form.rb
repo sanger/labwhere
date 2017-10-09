601e9a3cdb2935f692cc5be00cb5fd3d9cfb9585
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2017-10-09 17:07:19 +0100

    Refactoring location form

--- app/forms/location_form.rb
+++ app/forms/location_form.rb
@@ -2,57 +2,88 @@
 # Form object for a Location
 class LocationForm
 
-  include AuthenticationForm
-  include Auditor
+  include ActiveModel::Model
+  validate :check_user, :check_location, :check_range
+  attr_reader :current_user, :controller, :action, :location, :start_from, :end_to
 
-  validate :only_same_team_can_release_location
-
-  set_attributes :name, :location_type_id, :parent_id, :container, :status, :rows, :columns
-
-  after_assigning_model_variables :transform_location, :set_team
-
-  delegate :parent, :barcode, :parentage, :type, :coordinateable?, :reserved?, :reserved_by, to: :location
-  attr_writer :coordinateable, :reserve
-  attr_reader :start_from, :end_to
-
-  def coordinateable
-    @coordinateable ||= coordinateable?
+  def initialize(location = nil)
+    @location = location || Location.new
   end
 
-  def reserve
-    @reserve ||= reserved?
+  def submit(params)
+    assign_attributes(params)
+    if valid?
+      params.delete[:start_from]
+      params.delete[:end_to]
+      location.assign_attributes(params[:location])
+      transform_location
+      location.save if valid?
+    end
   end
 
   def destroy(params)
-    self.form_variables.assign(self, params)
+    assign_attributes(params)
     return false unless valid?
     location.destroy
     if location.destroyed?
       true
     else
-      add_errors
+      add_location_errors
       false
     end
   end
 
+  def check_user
+    UserValidator.new.validate(self) 
+  end
 
-private
+  def check_location
+    return if location.valid?
+    add_location_errors
+  end
+
+  def check_range
+    return if (start_from.empty? and end_to.empty?) or (not start_from.empty? and not end_to.empty?)
+    errors.add(:start_from, :blank, message: "must be present if End is present") if start_from.empty? 
+    errors.add(:end_to, :blank, message: "must be present if Start is present") if end_to.empty? 
+  end
+
+  private
+
+  def assign_attributes(params)
+    @current_user = User.find_by_code(params[:user_code])
+    @controller = params[:controller]
+    @action = params[:action]
+  end
+
+  def add_location_errors
+    location.errors.each do |key, value|
+      errors.add key, value
+    end
+  end
 
   def transform_location
-    @model = location.transform if location.new_record?
+    @location = location.transform if location.new_record?
   end
 
-  def set_team
-    @model.team_id = reserve_param? ? current_user.team_id : nil
+  def generate_names(prefix, start_from, end_to, &block)
+    if pos_int?(start_from) and pos_int?(end_to) and start_from.to_i < end_to.to_i
+      (prefix + start_from..prefix + end_to).each do |name|
+        yield name
+      end
+    end
   end
 
-  def only_same_team_can_release_location
-    return unless params.has_key? :location
-    LocationReleaseValidator.new(team_id: current_user.team_id).validate(self) if !reserve_param?
+  def run_transaction(&block)
+    begin
+      ActiveRecord::Base.transaction do
+        yield
+      end
+      true
+    rescue
+      false
+    end
   end
 
-  def reserve_param?
-    params.fetch(:location).fetch(:reserve, "0") == "1"
-  end
 
 end
--- spec/forms/location_form_spec.rb
+++ spec/forms/location_form_spec.rb
@@ -1,39 +1,66 @@
 require "rails_helper"
 
-RSpec.describe LocationTypeForm, type: :model do 
+RSpec.describe LocationForm, type: :model do 
 
-  let(:controller_params)           { { controller: "location", action: "create"} }
-  let(:params)                      { ActionController::Parameters.new(controller_params) }
-  let!(:administrator)              { create(:administrator) }
-  let(:location_params)             { attributes_for(:location).merge(user_code: administrator.barcode)}
-  let(:unordered_location_params)   { attributes_for(:unordered_location).merge(user_code: administrator.barcode)}
-  let(:ordered_location_params)     { attributes_for(:ordered_location).merge(user_code: administrator.barcode)}
-  let!(:parent_locations)           { create_list(:location, 2)}
+  let(:params) { attributes_for(:location) }
+  let(:controller_params) { { controller: "location", action: "create"} }
+  let!(:administrator) { create(:administrator) }
 
-  it "should be able to add a parent location using a barcode" do
+  it "is not valid without an user" do
     location_form = LocationForm.new
-    expect(location_form.submit(params.merge(location: location_params.merge(parent_id: parent_locations.first.id)))).to be_truthy
-    expect(location_form.parent).to eq(parent_locations.first)
+    location_form.submit(controller_params.merge(location: params))
+    expect(location_form).to_not be_valid
   end
 
-
-  it "should create the correct type of location dependent on the attributes" do
+  it "is not valid unless the location is valid" do
     location_form = LocationForm.new
-    location_form.submit(params.merge(location: unordered_location_params))
-    expect(location_form.location).to be_unordered
-    expect(location_form.location.coordinates).to be_empty
-
-    location_form = LocationForm.new
-    location_form.submit(params.merge(location: ordered_location_params))
-    expect(location_form.location).to be_ordered
-    expect(location_form.location.coordinates.count).to eq(create(:ordered_location).coordinates.count)
+    location_form.submit(controller_params.merge(location: params.except(:name), user_code: administrator.barcode))
+    expect(location_form).to_not be_valid
   end
 
-  it "should only destroy a location if it has not been used" do
+  it "is valid creates a new location" do
+    location_form = LocationForm.new
+    location_form.submit(controller_params.merge(location: params, user_code: administrator.barcode))
+    expect(location_form).to be_valid
+    expect(location_form.location).to be_persisted
+  end
+
+  it "can can be edited if exists" do
+    location = create(:location)
+    location_form = LocationForm.new(location)
+    new_location = build(:location)
+    location_form.submit(controller_params.merge(location: { name: new_location.name }, user_code: administrator.barcode, action: "update"))
+    expect(location.name).to eq(new_location.name)
+  end 
+
+  it "can be destroyed if it has not been used" do
     controller_params = { controller: "location", action: "create"} 
     params = ActionController::Parameters.new(controller_params)
     location = create(:location, name: 'Test Location')
     location_form = LocationForm.new(location)
     expect(location_form.destroy(params.merge(user_code: administrator.barcode))).to be_truthy
   end
+
+  it "should create the correct type of location dependent on the attributes" do
+    location_form = LocationForm.new
+    location_form.submit(controller_params.merge(location: attributes_for(:unordered_location), user_code: administrator.barcode))
+    expect(location_form.location).to be_unordered
+    expect(location_form.location.coordinates).to be_empty
+
+    location_form = LocationForm.new
+    location_form.submit(controller_params.merge(location: attributes_for(:ordered_location), user_code: administrator.barcode))
+    expect(location_form.location).to be_ordered
+    expect(location_form.location.coordinates.count).to eq(create(:ordered_location).coordinates.count)
+  end
+  
+  describe "multiple locations creation" do
+    it "should create multiple locations if start and end are not empty" do
+      location_form = LocationForm.new
+      location_form.submit(controller_params.merge(location: params.merge(start_from: "1", end_to: "4"), user_code: administrator.barcode))
+      expect(location_form).to be_valid
+    end
+
+    it "should not create multiple locations if start or end are empty"
+  end
+
 end
