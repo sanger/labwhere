dcb85b708ed2a577c0eb31cdeb13356501114c8a
Chris Smith <cs24@sanger.ac.uk>
2019-09-30 14:07:45 +0100

    Load as many associated models as possible in Api::Labwares::SearchesController

--- app/controllers/api/labwares/searches_controller.rb
+++ app/controllers/api/labwares/searches_controller.rb
@@ -1,8 +1,21 @@
 class Api::Labwares::SearchesController < ApiController
 
   def create
-    render json: Labware.includes([{ location: [{ coordinates: [:labware, :location] }] }, :coordinate])
-                        .by_barcode(params[:barcodes]), include: ["location.coordinates"]
+    # Little confusing, but the includes here is having to eager load as many associated models as possible
+    render json: Labware.includes([{ location: location_includes }, coordinate: coordinate_includes ])
+                     .by_barcode(params[:barcodes]), include: ["location.coordinates"]
+  end
+
+private
+
+  # Include all the models a location needs to eager load
+  def location_includes
+    [{coordinates: [:labware, :location]}]
+  end
+
+  # Include all the models a coordinate needs to eager load
+  def coordinate_includes
+    [location: location_includes]
   end
 
 end
