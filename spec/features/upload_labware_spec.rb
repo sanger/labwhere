8744626027f8d91b4a25c0355c064772637c8c08
Katy Taylor <kt17@sanger.ac.uk>
2020-06-11 15:23:44 +0100

    fix test

--- spec/features/upload_labware_spec.rb
+++ spec/features/upload_labware_spec.rb
@@ -5,6 +5,15 @@ require 'rails_helper'
 RSpec.describe 'UploadLabware', type: :feature do
   let!(:user) { create(:scientist) }
 
+  before do
+    location_type = LocationType.create!(name: 'test_location_type')
+    parent_location = UnorderedLocation.create!(name: 'parent location', location_type: location_type)
+    box_1 = UnorderedLocation.create!(name: 'box 1', location_type: location_type, parent: parent_location)
+    box_2 = UnorderedLocation.create!(name: 'box 2', location_type: location_type, parent: parent_location)
+    box_1.update!(barcode: 'BX1234')
+    box_2.update!(barcode: 'BX5678')
+  end
+
   it 'allows a user to upload a file' do
     visit new_upload_labware_path
     fill_in 'User swipe card id/barcode', with: user.swipe_card_id
