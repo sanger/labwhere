a413aac4ca5126b100153394d36c6e68168543a7
Stephen Inglis <si5@sanger.ac.uk>
2020-07-20 14:28:30 +0100

    Rubocopped.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 # config/initializers/cors.rb
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    resource '*', headers: :any, methods: [:get]
   end
 end
