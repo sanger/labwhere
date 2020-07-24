7677d1ea4e9cd2f8c92c091f77a5efd318ef2a51
Katy Taylor <kt17@sanger.ac.uk>
2020-07-24 13:57:58 +0100

    Add new endpoint to get all labwares at locations as specified by location barcodes - GET operation - index action - e.g. /api/labwares?location_barcodes=abc,def,ghi - returns list of labware objects

--- app/controllers/api/labwares_controller.rb
+++ app/controllers/api/labwares_controller.rb
@@ -5,9 +5,23 @@ class Api::LabwaresController < ApiController
     render json: current_resource
   end
 
+  def index
+    # doesn't currently include a basic index function, returning all labwares, but could in future
+    render json: labwares_by_location
+  end
+
   private
 
   def current_resource
     Labware.find_by_code(params[:barcode]) if params[:barcode]
+  end
+
+  def labwares_by_location
+    # expects url param of location barcode(s)
+    # for example, ?location_barcodes=lw-uk-biocentre-mk-6-3662,lw-reefer-1-33
+    location_barcodes = params[:location_barcodes]
+    return unless location_barcodes
+
+    Labware.joins(:location).where(locations: { barcode: location_barcodes.split(',') })
   end
 end
