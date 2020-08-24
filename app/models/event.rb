8905d13cc125f1f98f171501f4e08dae3cfa09d9
Katy Taylor <kt17@sanger.ac.uk>
2020-08-24 10:19:56 +0100

    rubocop + change fake uuids to numbers so they don't get rejected the other end - messages are now being inserted successfully into the UAT event warehouse

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,29 +15,29 @@ class Event
   def as_json(*)
     {
       event: {
-        uuid: 'TODO',
+        uuid: '1234',
         event_type: action,
-        occured_at: Time.now,
+        occured_at: Time.zone.now,
         user_identifier: user.login,
         subjects: [
           {
             role_type: 'labware',
             subject_type: 'labware',
             friendly_name: labware.barcode,
-            uuid: 'TODO'
+            uuid: '5678'
           },
           {
             role_type: 'location',
             subject_type: 'location',
             friendly_name: location.barcode,
-            uuid: 'TODO'
+            uuid: '9101'
           }
         ],
         metadata: {
           location_barcode: location.barcode,
           location_coordinate: coordinate.try(:position),
           location_name: location.name,
-          location_parentage: location.parentage,
+          location_parentage: location.parentage
         }
       },
       lims: 'LABWHERE'
