bdb23df29c1397a22910ad0c3e4a40095df1db1d
Katy Taylor <kt17@sanger.ac.uk>
2020-08-25 14:05:51 +0100

    fix and improve the event spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe Event, type: :model do
   let(:user) { create(:user) }
   let(:labware) { create(:labware_with_location) }
   let(:action) { 'scanned' }
-  let(:attributes) { { user: user, labware: labware, action: action } }
+  let(:audit) { create(:audit) }
+  let(:attributes) { { user: user, labware: labware, action: action, audit: audit } }
 
   it 'must have a user' do
     expect(Event.new(attributes.except(:user))).to_not be_valid
@@ -26,36 +27,65 @@ RSpec.describe Event, type: :model do
 
   it 'may have a coordinate' do
     coordinate = create(:coordinate, labware: create(:labware))
-    audit = Event.new(attributes.merge(labware: coordinate.labware))
-    expect(audit.coordinate).to be_present
+    event = Event.new(attributes.merge(labware: coordinate.labware))
+    expect(event.coordinate).to be_present
   end
 
   context 'for an unordered location' do
     let(:location) { create(:unordered_location_with_parent) }
     let(:labware) { create(:labware, location: location) }
+    let(:date_time) { Time.zone.now }
+    let(:expected_json) do
+      {
+        event: {
+          uuid: audit.uuid,
+          event_type: action,
+          occured_at: date_time,
+          user_identifier: user.login,
+          subjects: [
+            {
+              role_type: 'labware',
+              subject_type: 'labware',
+              friendly_name: labware.barcode,
+              uuid: labware.uuid
+            },
+            {
+              role_type: 'location',
+              subject_type: 'location',
+              friendly_name: location.barcode,
+              uuid: location.uuid
+            }
+          ],
+          metadata: {
+            location_barcode: location.barcode,
+            location_coordinate: labware.coordinate.position,
+            location_name: location.name,
+            location_parentage: location.parentage
+          }
+        },
+        lims: 'LABWHERE'
+      }
+    end
+
+    before { allow(Time.zone).to receive(:now).and_return(date_time) }
 
     it 'will produce the correct json for the message' do
-      audit = Event.new(user: user, labware: labware, action: action)
-      json = audit.as_json
-      expect(json[:location_barcode]).to eq(location.barcode)
-      expect(json[:location_name]).to eq(location.name)
-      expect(json[:location_parentage]).to eq(location.parentage)
-      expect(json[:labware_barcode]).to eq(labware.barcode)
-      expect(json[:action]).to eq(action)
-      expect(json[:user_login]).to eq(user.login)
-      expect(json[:location_coordinate]).to be_nil
+      event = Event.new(user: user, labware: labware, action: action, audit: audit)
+      json = event.as_json
+      expect(json).to eq(expected_json)
     end
   end
 
   context 'for an ordered location' do
-    coordinate = create(:coordinate, labware: create(:labware))
+    let(:coordinate) { create(:coordinate, labware: create(:labware)) }
 
     it 'will produce the correct json for the message' do
-      location = coordinate.labware.location
-      audit = Event.new(attributes.merge(labware: coordinate.labware))
-      json = audit.as_json
-      expect(json[:location_barcode]).to eq(location.barcode)
-      expect(json[:location_coordinate]).to eq(coordinate.position)
+      locn = coordinate.labware.location
+      event = Event.new(attributes.merge(labware: coordinate.labware))
+      json = event.as_json
+
+      expect(json[:event][:metadata][:location_barcode]).to eq(locn.barcode)
+      expect(json[:event][:metadata][:location_coordinate]).to eq(coordinate.position)
     end
   end
 end
