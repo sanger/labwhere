1c09faa0e2d38a6787a079694193a7e76693ec18
Katy Taylor <kt17@sanger.ac.uk>
2020-08-26 13:51:22 +0100

    added comments to uuid columns

--- db/migrate/20200824095014_add_uuids_to_labware_and_location.rb
+++ db/migrate/20200824095014_add_uuids_to_labware_and_location.rb
@@ -1,7 +1,7 @@
 class AddUuidsToLabwareAndLocation < ActiveRecord::Migration[5.2]
   def change
-    add_column :labwares, :uuid, :string, limit: 36
-    add_column :locations, :uuid, :string, limit: 36
-    add_column :audits, :uuid, :string, limit: 36
+    add_column :labwares, :uuid, :string, limit: 36, comment: "Unique identifier for this Labware. Added to send to Events Warehouse. Doesn't match up with Sequencescape or any other app."
+    add_column :locations, :uuid, :string, limit: 36, comment: 'Unique identifier for this Location. Added to send to Events Warehouse.'
+    add_column :audits, :uuid, :string, limit: 36, comment: 'Unique identifier for this Audit. Added to send to Events Warehouse.'
   end
 end
