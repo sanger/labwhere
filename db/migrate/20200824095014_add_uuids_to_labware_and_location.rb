a8391e46f3ec5145a7e8fe302236dbee7b2ca19b
Katy Taylor <kt17@sanger.ac.uk>
2020-08-26 15:36:35 +0100

    Add null: false to the uuid columns - Can get around it in SQLite (development mode) by recreating the database, and works in MySQL

--- db/migrate/20200824095014_add_uuids_to_labware_and_location.rb
+++ db/migrate/20200824095014_add_uuids_to_labware_and_location.rb
@@ -1,7 +1,12 @@
 class AddUuidsToLabwareAndLocation < ActiveRecord::Migration[5.2]
+  # Limitations with running this migration while using SQLite3 (e.g. in development mode):
+  # null: false will error if you have existing data in the tables
+  # limit and comment are not implemented
+  # Do a db:drop and db:create to get around it (if in development mode!)
+  # Migration works fine with MySQL
   def change
-    add_column :labwares, :uuid, :string, limit: 36, comment: "Unique identifier for this Labware. Added to send to Events Warehouse. Doesn't match up with Sequencescape or any other app."
-    add_column :locations, :uuid, :string, limit: 36, comment: 'Unique identifier for this Location. Added to send to Events Warehouse.'
-    add_column :audits, :uuid, :string, limit: 36, comment: 'Unique identifier for this Audit. Added to send to Events Warehouse.'
+    add_column :labwares, :uuid, :string, limit: 36, null: false, comment: "Unique identifier for this Labware. Added to send to Events Warehouse. Doesn't match up with Sequencescape or any other app."
+    add_column :locations, :uuid, :string, limit: 36, null: false, comment: 'Unique identifier for this Location. Added to send to Events Warehouse.'
+    add_column :audits, :uuid, :string, limit: 36, null: false, comment: 'Unique identifier for this Audit. Added to send to Events Warehouse.'
   end
 end
