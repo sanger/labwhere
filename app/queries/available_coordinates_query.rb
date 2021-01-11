47563b4921de0e756e3e0f40d2b534d00449f701
Stephen Inglis <si5@sanger.ac.uk>
2021-01-11 15:24:05 +0000

    Rubocopped.

--- app/queries/available_coordinates_query.rb
+++ app/queries/available_coordinates_query.rb
@@ -6,7 +6,7 @@ class AvailableCoordinatesQuery
   def self.call(locations = Location.all, min_available_coordinates)
     locations.joins(%q{INNER JOIN coordinates ON coordinates.location_id = locations.id
                        LEFT OUTER JOIN labwares ON labwares.coordinate_id = coordinates.id})
-             .where("labwares.coordinate_id is NULL")
+             .where(labwares: { coordinate_id: nil })
              .group("locations.id")
              .having("COUNT(*) >= ?", min_available_coordinates.to_i)
   end
