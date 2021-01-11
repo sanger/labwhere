f2fae99878c367e0998f01c77964f638889a39ed
Stephen Inglis <si5@sanger.ac.uk>
2021-01-11 15:05:49 +0000

    removed multiple strips.

--- app/models/manifest_uploader.rb
+++ app/models/manifest_uploader.rb
@@ -10,7 +10,12 @@ class ManifestUploader
   validate :check_locations, :check_duplicate_positions, :check_positions_valid
 
   def data
-    @data ||= ::CSV.parse(file).drop(1)
+    @data ||= format_data
+  end
+
+  def format_data
+    parsed = ::CSV.parse(file).drop(1)
+    parsed.collect { |row| row.collect { |cell| cell.try(:strip) } }
   end
 
   def run
@@ -19,9 +24,9 @@ class ManifestUploader
     ActiveRecord::Base.transaction do
       data.each do |row|
         location_barcode, labware_barcode, position = row
-        labware = Labware.find_or_initialize_by(barcode: labware_barcode.strip)
-        labware.location = locations[location_barcode.strip]
-        labware.coordinate = stored_coordinates["#{location_barcode.strip}, #{position.strip}"] unless position.nil?
+        labware = Labware.find_or_initialize_by(barcode: labware_barcode)
+        labware.location = locations[location_barcode]
+        labware.coordinate = stored_coordinates["#{location_barcode}, #{position}"] unless position.nil?
         labware.save!
         labware.create_audit!(user, Audit::MANIFEST_UPLOAD_ACTION)
       end
@@ -33,7 +38,7 @@ class ManifestUploader
   end
 
   def location_barcodes
-    @location_barcodes ||= data.collect { |item| item.first.strip }.uniq
+    @location_barcodes ||= data.collect { |item| item.first }.uniq
   end
 
   def stored_coordinates
@@ -47,11 +52,11 @@ class ManifestUploader
   end
 
   def missing_locations
-    @missing_locations ||= location_barcodes.reject { |barcode| locations.key?(barcode.strip) }
+    @missing_locations ||= location_barcodes.reject { |barcode| locations.key?(barcode) }
   end
 
   def ordered_location_barcodes
-    @ordered_location_barcodes ||= location_barcodes.select { |barcode| locations[barcode.strip].type == "OrderedLocation" if locations.include?(barcode.strip) }
+    @ordered_location_barcodes ||= location_barcodes.select { |barcode| locations[barcode].type == "OrderedLocation" if locations.include?(barcode) }
   end
 
   def ordered_location_rows
@@ -61,7 +66,7 @@ class ManifestUploader
   def find_ordered_location_rows
     rows = []
     data.each_with_index do |row, index|
-      location_barcode = row.first.strip
+      location_barcode = row.first
       if ordered_location_barcodes.include?(location_barcode)
         indexed_row = [(index + 2).to_s] + row
         rows.push(indexed_row)
@@ -87,18 +92,18 @@ class ManifestUploader
 
   def check_positions_valid
     ordered_location_rows.each do |row|
-      line_index, location_barcode, labware_barcode, position = row.collect(&:strip)
+      line_index, location_barcode, labware_barcode, position = row
 
       if position.nil? || !valid_number?(position)
         errors.add(:base, "Line #{line_index}: invalid entry for position. Please specify a positive integer.")
       else
-        coordinate = Coordinate.find_by(location_id: locations[location_barcode.strip].id, position: position)
+        coordinate = Coordinate.find_by(location_id: locations[location_barcode].id, position: position)
         if coordinate.nil?
           errors.add(:base, "Line #{line_index}: target position #{position} for location with barcode #{location_barcode} does not exist")
         elsif coordinate.filled?
           errors.add(:base, "Line #{line_index}: target position #{position} for labware with barcode #{labware_barcode} is already occupied")
         else
-          stored_coordinates["#{location_barcode.strip}, #{position.strip}"] = coordinate
+          stored_coordinates["#{location_barcode}, #{position}"] = coordinate
         end
       end
     end
