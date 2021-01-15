e8a9a79732a7b00dcbd4dda05af2de45ff970108
Christopher Friend <christopher.friend@tessella.com>
2021-01-15 10:06:35 +0000

    Rubocop fixes

--- app/lib/utils/barcode_utilities.rb
+++ app/lib/utils/barcode_utilities.rb
@@ -4,7 +4,7 @@ module BarcodeUtilities
   ##
   # Take the barcodes for the object and create a string using the defined delimiter.
   def join_barcodes(char = "\n")
-    extract_barcodes().join(char)
+    extract_barcodes.join(char)
   end
 
   # Extract the barcodes from the input object(s)
