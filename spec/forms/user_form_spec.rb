35303a969736c1a2f7edd7146100e53beaa11358
Ben Topping <ben.topping@outlook.com>
2021-02-09 11:01:01 +0000

    removed ambiguous block association check from rubocop

--- spec/forms/user_form_spec.rb
+++ spec/forms/user_form_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe UserForm, type: :model do
   subject { UserForm.new(technician) }
 
   it "if user is unauthorised (scientist) then swipe card id should not be updated" do
-    expect { # rubocop:todo Lint/AmbiguousBlockAssociation
+    expect {
       UserForm.new(scientist).submit(params.merge(user: { swipe_card_id: '12345', user_code: sci_swipe_card_id }, commit: "Update User"))
     }.to_not change { technician.reload.swipe_card_id }
   end
@@ -27,13 +27,13 @@ RSpec.describe UserForm, type: :model do
   end
 
   it "if user is authorised but swipe card id is left blank then swipe card id should not be updated" do
-    expect { # rubocop:todo Lint/AmbiguousBlockAssociation
+    expect {
       subject.submit(params.merge(user: { swipe_card_id: nil, user_code: admin_swipe_card_id }, commit: "Update User"))
     }.to_not change { technician.reload.swipe_card_id }
   end
 
   it "if user is authorised but barcode is left blank then barcode should not be updated" do
-    expect { # rubocop:todo Lint/AmbiguousBlockAssociation
+    expect {
       subject.submit(params.merge(user: { barcode: nil, user_code: admin_swipe_card_id }, commit: "Update User"))
     }.to_not change { technician.reload.barcode }
   end
