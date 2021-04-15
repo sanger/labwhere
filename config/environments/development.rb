b222f241ec95825a761ff6a4a1fa0e1c380cc9a0
Eduardo Martin Rojo <emr@sanger.ac.uk>
2021-04-15 13:50:00 +0100

    Rubocop

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,7 +68,6 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
