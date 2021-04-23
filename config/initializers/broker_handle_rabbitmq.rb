41e79576c190b29b8c2dc683b7ccc2c807d6bf62
Eduardo Martin Rojo <emr@sanger.ac.uk>
2021-04-23 12:25:37 +0100

    Rubocop

--- config/initializers/broker_handle_rabbitmq.rb
+++ config/initializers/broker_handle_rabbitmq.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Broker setup
 Broker::Handle = Messages::Broker.new(Rails.configuration.bunny)
 Broker::Handle.create_connection if Rails.configuration.bunny['enabled']
