c91ec36031ebb04b830eb02342cff23791155df9
Stephen Inglis <519327+stevieing@users.noreply.github.com>
2021-08-04 11:53:47 +0100

    Fixed rubocop errors.

--- app/lib/validators/parent_white_list_validator.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class ParentWhiteListValidator < ActiveModel::Validator
-  def validate(record)
-    unless record.parent.present? and record.parent.location_type.in?(options[:location_types])
-      record.errors[:parent] << "must be one of the following types: #{options[:location_types].map(&:name).join(", ")}"
-    end
-  end
-end
