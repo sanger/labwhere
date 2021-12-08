aaa1147c4c9e1985265d7a614fc4da173edac167
Harriet Craven <hc6@sanger.ac.uk>
2021-12-08 11:10:09 +0000

    rubocopped

--- app/messages/messages/broker.rb
+++ app/messages/messages/broker.rb
@@ -10,7 +10,7 @@ module Messages
     attr_reader :channel, :exchange, :bunny_config
 
     def initialize(bunny_config)
-      @bunny_config = OpenStruct.new(bunny_config)
+      @bunny_config = OpenStruct.new(bunny_config) # rubocop:todo Style/OpenStructUse
     end
 
     # This module provides a way of performing the create_connection call only
