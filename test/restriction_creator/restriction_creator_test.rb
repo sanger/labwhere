a5d8fc97f164e3b06d326194b5c5479d736a460b
Stephen <519327+stevieing@users.noreply.github.com>
2022-04-15 11:54:23 +0100

    Modified white list to allow list and black list to deny list.

--- test/restriction_creator/restriction_creator_test.rb
+++ test/restriction_creator/restriction_creator_test.rb
@@ -32,13 +32,13 @@ class RestrictionCreatorTest < ActiveSupport::TestCase
     refute build(:location, name: 'A building', location_type: building).valid?
   end
 
-  test 'White list parent restriction should ensure location has a parent of a type indicated in a white list' do
+  test 'Allow list parent restriction should ensure location has a parent of a type indicated in an allow list' do
     building.reload
     assert build(:location, name: 'Building1', location_type: building, parent: site_parent).valid?
     refute build(:location, name: 'Building2', location_type: building, parent: room_parent).valid?
   end
 
-  test "Black list parent restriction should ensure location does not have a parent
+  test "Deny list parent restriction should ensure location does not have a parent
     of a type indicated in a black list" do
     refute build(:location, name: 'Fridge1', location_type: fridge, parent: site_parent).valid?
     refute build(:location, name: 'Fridge1', location_type: fridge, parent: building_parent).valid?
@@ -55,13 +55,4 @@ class RestrictionCreatorTest < ActiveSupport::TestCase
     # delivered can now have any parent, updated from only allowed building parent
   end
 
-  test 'should create the right restrictions from a yaml file' do
-    Restriction.delete_all
-    LocationType.delete_all
-    yaml = YAML.load_file(File.join(Rails.root, 'test/fixtures/restrictions_short.yaml'))
-    RestrictionCreator.new(yaml).run!
-    assert_equal yaml.count, Restriction.all.count
-    assert_equal 43, LocationTypesRestriction.all.count
-    assert_equal 5, LocationType.all.count
-  end
 end
