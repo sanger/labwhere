c9d5082a6acf2cc46ff29bb77a2bdefc5a7499ec
yoldas <ay6@sanger.ac.uk>
2024-06-07 01:24:51 +0100

    Assert that the rake task increments the number of locations in the test

--- spec/lib/tasks/destroyed_location_spec.rb
+++ spec/lib/tasks/destroyed_location_spec.rb
@@ -6,10 +6,16 @@ require 'rake'
 RSpec.describe 'destroyed_location:create' do
   before do
     Rails.application.load_tasks
+    # The rake task assumes that the parent location with the name 'Sanger' and
+    # the location type name 'Site' already exists.
+    parent_type = create(:location_type, name: 'Site')
+    create(:unordered_location, name: 'Sanger', location_type: parent_type)
   end
 
   it 'creates the Destroyed LocationType and Location' do
-    Rake::Task['destroyed_location:create'].invoke
+    expect do
+      Rake::Task['destroyed_location:create'].invoke
+    end.to change { UnorderedLocation.count }.by(1)
 
     location_type = LocationType.find_by(name: 'Destroyed')
     expect(location_type).not_to be_nil
