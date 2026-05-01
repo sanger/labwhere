4e2a4c30997d12eddc640c40ede8e42c57cc52bc
Ben Topping <ben.topping@outlook.com>
2026-05-01 13:07:48 +0100

    docs: correct comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,9 @@
 
 # ApplicationController - base for all controllers
 class ApplicationController < ActionController::Base
-  # LabWhere is not vulnerable to CSRF attacks as it does not have user logins or sessions
-  # but we want to handle the issues gracefully if an idle user tries to perform an action after the session has expired
+  # LabWhere is not vulnerable to CSRF attacks as it does not have user logins or persisted sessions
+  # but we want to handle the issues gracefully if an idle user tries to perform an
+  # action after the authenticity_token has expired
   protect_from_forgery with: :reset_session
 
   # Reset the connection check for every request so it will only
